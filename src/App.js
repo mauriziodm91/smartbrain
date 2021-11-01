0dbf6af64e3f7c46ca49f0cae5a68844fc891694
maurizio <mauriziodm91@gmail.com>
2021-11-01 07:13:23 -0300

    adding serve

--- src/App.js
+++ src/App.js
@@ -174,7 +174,7 @@ class App extends Component{
 
   onButtonSubmit= ()=>{
     this.setState({imageUrl: this.state.input})
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://shocking-goosebumps-33257.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -184,7 +184,7 @@ class App extends Component{
       .then(response => response.json())
       .then(response => {
         if(response){
-          fetch('http://localhost:3000/image', {
+          fetch('https://shocking-goosebumps-33257.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
